b4f0dab7e056360ebe4ac8097adc937b9287a06c
Emmanuel John Ayarma <johnayarmahe@gmail.com>
2025-03-29 21:05:47 +0000

    fix(db): Resolve seeding error

--- database/seeders/WorldSeeder.php
+++ database/seeders/WorldSeeder.php
@@ -7,11 +7,10 @@ use Nnjeim\World\Actions\SeedAction;
 
 class WorldSeeder extends Seeder
 {
-    public function run()
-    {
-        ini_set('memory_limit', '1024M');
-        $this->call([
-            SeedAction::class,
-        ]);
-    }
+	public function run()
+	{
+		$this->call([
+			SeedAction::class,
+		]);
+	}
 }
