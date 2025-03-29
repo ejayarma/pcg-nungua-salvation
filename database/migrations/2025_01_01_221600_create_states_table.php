d982b8f49c6c2c2f02f0ac1bf14be4cf55a0e190
Emmanuel John Ayarma <johnayarmahe@gmail.com>
2025-03-29 22:49:11 +0000

    feat(db): Seed states manually

--- database/migrations/2025_01_01_221600_create_states_table.php
+++ database/migrations/2025_01_01_221600_create_states_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('states', function (Blueprint $table) {
             $table->id();
-
             $table->string('name')->unique();
             $table->timestamps();
         });
